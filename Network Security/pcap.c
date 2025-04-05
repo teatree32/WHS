08e2a033c6842b45c914906ec170f1123605afe0
teatree32 <164837312+teatree32@users.noreply.github.com>
2025-04-06 00:55:28 +0900

    PCAP Programming

--- Network Security/pcap.c
+++ Network Security/pcap.c
@@ -9,20 +9,20 @@
 void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet) {
     struct ethheader *eth = (struct ethheader *)packet;
 
-    // IP 패킷인지 확인
+    // IP 패킷
     if (ntohs(eth->ether_type) == 0x0800) {
         struct ipheader *ip = (struct ipheader *)(packet + sizeof(struct ethheader));
 
         // IP header 길이 계산 (단위: 4바이트 -> 바이트로 변환)
         int ip_header_len = ip->iph_ihl * 4;
 
-        // TCP 패킷인지 확인
+        // TCP 패킷
         if (ip->iph_protocol == IPPROTO_TCP) {
             struct tcpheader *tcp = (struct tcpheader *)((u_char *)ip + ip_header_len);
 
             int tcp_header_len = TH_OFF(tcp) * 4;
 
-            // 데이터(payload) 시작 위치
+            // 데이터 시작 위치
             const u_char *data = (u_char *)tcp + tcp_header_len;
             int data_len = ntohs(ip->iph_len) - ip_header_len - tcp_header_len;
 
@@ -37,7 +37,6 @@ void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *pa
             printf("IP Header: src ip %s | dst ip %s\n", inet_ntoa(ip->iph_sourceip), inet_ntoa(ip->iph_destip));
             printf("TCP Header: src port: %d | dst port: %d\n", ntohs(tcp->tcp_sport), ntohs(tcp->tcp_dport));
 
-            // 메시지 출력 (길이 체크)
             if (data_len > 0 && data_len < 1500) {
                 printf("Message (%d bytes):\n", data_len);
                 for (int i = 0; i < data_len; i++) {
@@ -56,13 +55,13 @@ int main() {
     pcap_t *handle;
     char errbuf[PCAP_ERRBUF_SIZE];
     struct bpf_program fp;
-    char filter_exp[] = "tcp"; // TCP만 캡처
+    char filter_exp[] = "tcp"; // TCP만 출력
     bpf_u_int32 net;
 
-    // pcap 세션 시작
+    // pcap 세션 시작 (on NIC with name enp33)
     handle = pcap_open_live("ens33", BUFSIZ, 1, 1000, errbuf);
     if (handle == NULL) {
-        fprintf(stderr, "Couldn't open device: %s\n", errbuf);
+        fprintf(stderr, "Error: %s\n", errbuf);
         return 2;
     }
 
@@ -70,7 +69,7 @@ int main() {
     pcap_compile(handle, &fp, filter_exp, 0, net);
     pcap_setfilter(handle, &fp);
 
-    // 패킷 캡처 시작
+    // 패킷 캡처
     pcap_loop(handle, -1, got_packet, NULL);
 
     pcap_close(handle);
